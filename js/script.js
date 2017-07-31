33df0e3024ed0a3650f79a3bd3bda574ac4667c1
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-07-31 12:06:25 -0400

    mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
 
 $(document).ready(function() {
+  $('#hamburger_nav').hide();
+
+  $('#hamburger').click(function(event) {
+    event.preventDefault();
+    $('#hamburger_nav').slideToggle();
+  });
+
   $("#home-nav").click(function() {
-      $('body').animate({scrollTop: $("#home").offset().top - 120}, "slow");
-      return false;
+    $('body').animate({scrollTop: $("#home").offset().top - 120}, "slow");
+    return false;
   });
 
   $("#about-nav").click(function() {
-      $('body').animate({scrollTop: $("#about").offset().top - 120}, "slow");
-      return false;
+    $('body').animate({scrollTop: $("#about").offset().top - 120}, "slow");
+    return false;
   });
 
   $("#work-nav").click(function() {
-      $('body').animate({scrollTop: $("#work").offset().top - 120}, "slow");
-      return false;
+    $('body').animate({scrollTop: $("#work").offset().top - 120}, "slow");
+    return false;
   });
 
   $("#contact-nav").click(function() {
-      $('body').animate({scrollTop: $("#contact").offset().top - 120}, "slow");
-      return false;
+    $('body').animate({scrollTop: $("#contact").offset().top - 120}, "slow");
+    return false;
   });
 
 
